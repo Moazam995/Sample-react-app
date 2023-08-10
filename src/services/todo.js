aa5301f148e68b9a62ff531090c7f9af3e962995
Moazam993 <moazam.akhoond95@gmail.com>
2023-08-10 16:49:13 +0500

    change commit

--- src/services/todo.js
+++ src/services/todo.js
@@ -8,7 +8,7 @@ export function getAll() {
     return [
         {
             id: 1,
-            text: 'how to learn Javascript',
+            text: 'Javascript General Knowledge',
             completed: false
         },
         {

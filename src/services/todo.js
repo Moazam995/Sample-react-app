fd13ab2fb84217250d86935e64816f4c81d7fb55
Moazam993 <moazam.akhoond95@gmail.com>
2023-11-06 15:36:43 +0500

    change1

--- src/services/todo.js
+++ src/services/todo.js
@@ -8,7 +8,7 @@ export function getAll() {
     return [
         {
             id: 1,
-            text: 'Javascript Knowledge ',
+            text: 'Knowledged',
             completed: false
         },
         {

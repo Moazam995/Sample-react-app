48ed5a3afa089eda05121ce27485c82ca4bd6000
Moazam993 <moazam.akhoond95@gmail.com>
2023-11-06 23:41:32 +0500

    last change

--- src/services/todo.js
+++ src/services/todo.js
@@ -8,7 +8,7 @@ export function getAll() {
     return [
         {
             id: 1,
-            text: 'Knowledged',
+            text: 'Knowledged of Javascript',
             completed: false
         },
         {
